51265f4038ff749ef8ff8f5851a386979520e26d
zeande <zechariah.andersen@gmail.com>
2013-02-03 14:13:45 -0600

    Changed some of the dummy songs and changed spacing within queue.html

--- assets/includes/self_interaction.php
+++ assets/includes/self_interaction.php
@@ -18,8 +18,8 @@ function getSonicFlowResults($search) {
 	// This is a sample function that doesn't yet actually do anything.
 	$results = array();
 	$results[] = new Song(100, "Stein um Stein", "Rammstein", "Reise, Reise");
-	$results[] = new Song(101, "Stark", "Ich + Ich", "");
-	$results[] = new Song(102, "Ich + Ich", "Ich + Ich", "Ich + Ich");
+	$results[] = new Song(101, "Stark", "Ich + Ich", "Vom selben Stern");
+	$results[] = new Song(102, "Wave No Flag", "Mono Inc.", "After the War");
 	return $results;
 }
 
