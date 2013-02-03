c4cb67b66ef9c5335e3c91e01ae4299f54adfb7d
zeande <zechariah.andersen@gmail.com>
2013-02-03 12:55:24 -0600

    Added function to get album art from the gracenote.php file.

--- assets/includes/gracenote.php
+++ assets/includes/gracenote.php
@@ -2,4 +2,18 @@
 /**
  * Contains functions for interacting with data from the Gracenote data server.
  */
+
+require('config.php');
+require_once("./php-gracenote/Gracenote.class.php");
+
+$gracenoteAPI = new Gracenote\WebAPI\GracenoteWebAPI($config["gn_id"], $config["gn_tag"], $config["gn_user"]);
+
+function getArt($title, $artist, $album,$size="small") {
+	global $gracenoteAPI;
+
+	$results = $gracenoteAPI->searchTrack($artist,$title,$album, Gracenote\WEBAPI\GracenoteWebAPI::BEST_MATCH_ONLY);
+	$picLoc = $results[0]["album_art_url"];
+	$picLoc = str_replace("size=medium","size=$size",$picLoc);
+	return $picLoc;
+}
 ?>
