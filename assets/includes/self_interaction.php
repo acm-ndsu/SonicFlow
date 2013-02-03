8a3b4d80d09ab43766a0625704928fce6320763b
zeande <zechariah.andersen@gmail.com>
2013-02-03 16:52:28 -0600

    Added pgsql database interaction for searching SonicFlow.

--- assets/includes/self_interaction.php
+++ assets/includes/self_interaction.php
@@ -14,12 +14,32 @@
  * phrase.
  */
 function getSonicFlowResults($search) {
-	// TODO: Actually query the database.
-	// This is a sample function that doesn't yet actually do anything.
+	global $config;
+
+	$conn_string = "host=localhost dbname=%s user=%s password=%s";
+	$conn_string = sprintf($conn_string,$config["pg_db"],$config["pg_user"],$config["pg_pass"]);
+	$dbconn = pg_connect($conn_string);
+	$query = 'SELECT queue.gid,songs.artist,songs.title,songs.album,queue.arturl FROM queue,songs WHERE ';
+	$query .= 'queue.gid = songs.gid AND ( songs.title LIKE $1 OR songs.artist LIKE $1 )';
+	$result = pg_prepare($dbconn,"songs",$query);
+
+	$result = pg_execute($dbconn,"songs",array($search)) or die('Query failed: ' . pg_last_error());
 	$results = array();
-	$results[] = new Song(100, "Stein um Stein", "Rammstein", "Reise, Reise");
-	$results[] = new Song(101, "Stark", "Ich + Ich", "Vom selben Stern");
-	$results[] = new Song(102, "Wave No Flag", "Mono Inc.", "After the War");
+	while ($line = pg_fetch_array($result, null,PGSQL_ASSOC)) {
+		$results[] = new Song($line["gid"], $line["title"], $line["artist"], $line["album"], $line["arturl"]);
+	}
+
+	// Free resultset
+	pg_free_result($result);
+
+	// Close connection
+	pg_close($dbconn);
+/*
+	$results   = array();
+	$results[] = new Song(100, "Führe mich", "Rammstein", "Liebe ist für alle da", "");
+	$results[] = new Song(101, "Stark", "Ich + Ich", "Vom selben Stern", "");
+	$results[] = new Song(102, "Wave No Flag", "Mono Inc.", "After the War", "");
+*/
 	return $results;
 }
 
