0d654b2161dbb522b340ad7a3844834c4aeabdf1
TF Nelson <tails.fanatic@gmail.com>
2013-02-03 09:48:40 -0600

    Split search handler to depend on provider param

--- results.php
+++ results.php
@@ -3,6 +3,29 @@
 <?php
 	$provider = $_GET['provider'];
 	
-	$results = getSongResults($provider);
+	var $searchResults;
+	
+	switch ($provider) {
+		case 'sonicflow':
+			$searchResults = getSonicFlowResults();
+			break;
+			
+		case 'grooveshark':
+			$searchResults = getGroovesharkResults();
+			break;
+			
+		default:
+			$searchResults = NULL;
+			break;
+	}
+	
+	if (is_null($searchResults)) {
+		?>
+			<div class="errorMessage">
+				<p>Something went wrong. Please hit the back button and try
+				again.</p>
+			</div>
+		<?php
+	}
 ?>
 <?php include('footer.html'); ?>
