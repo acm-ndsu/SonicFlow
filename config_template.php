9d07dff551f59b2a751d09638f388690a6610830
zeande <zechariah.andersen@gmail.com>
2013-02-03 19:44:46 -0600

    Added a config.php template.

---
+++ config_template.php
@@ -0,0 +1,20 @@
+<?php
+	/* Rename this file to config.php when you set this up. */
+
+	/*
+	 * Gracenote client ID, client tag, and user id.
+	 * Without this information, you will not be able to query
+	 * Gracenote to get album artwork.
+	 */
+	$config["gn_id"]   = "XXXXXXXX";
+	$config["gn_tag"]  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	$config["gn_user"] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+	/*
+	 * Local postgres information.
+	 * username, password, and database name.
+	 */
+	$config["pg_user"] = "";
+	$config["pg_pass"] = "";
+	$config["pg_db"]   = "";
+?>
