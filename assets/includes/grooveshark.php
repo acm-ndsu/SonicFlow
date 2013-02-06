940e5fd8d119ea366aa66d702f1570e2020e3d64
zeande <zechariah.andersen@gmail.com>
2013-02-06 15:18:12 -0600

    Updated grooveshark.php to add songs to the database as it searches.

--- assets/includes/grooveshark.php
+++ assets/includes/grooveshark.php
@@ -14,29 +14,23 @@
  * phrase.
  */
 function getGroovesharkResults($search) {
-	// TODO: Actually query the database.
-	// This is a sample function that doesn't yet actually do anything.
 	$songs  = findSongs($search);
-	$insertArtist = 'INSERT INTO artists VALUES ($1,$2)';
-	$insertAlbum  = 'INSERT INTO albums VALUES ($1,$2,$3,$4)';
-	$insertSong   = 'INSERT INTO songs VALUES ($1,$2,$3)';
-	$songQuery    = 'SELECT id FROM songs WHERE id = $1';
-
-	$dbconn = pg_connect(getConnectionString());
-	$result = pg_prepare($dbconn,'checkSong',$songQuery);
-	pg_prepare($dbconn,'artist',$insertArtist);
-	pg_prepare($dbconn,'album', $insertAlbum);
-	pg_prepare($dbconn,'song',  $insertSong);
-
 	foreach ($songs as $song) {
-		$result = pg_execute($dbconn,'checkSong',array($song->id));
-		if (!pg_fetch_all($result)) {
-			pg_execute($dbconn,'artist',array($song->artistId,$song->artist));
-			pg_execute($dbconn,'album',array($song->albumId,$song->album,$song->artistId,$song->arturl));
-			pg_execute($dbconn,'song',array($song->id,$song->title,$song->albumId));
-		}	
+		if (!itemIsInDb('songs',$song->id)) {
+		{
+			if (!itemIsInDb('albums',$song->albumId)) {
+				if (!itemsIsInDb('artists',$song->artistId)) {
+					addArtist($song->artistId);
+				}
+				$location = 'assets/albumart/' . $song->albumId . '.jpg';
+				addAlbum($song->albumId,$song->albumName,$song->artistId,$location,$song->arturl);
+			}
+			addSong($song->id,$song->title,$song->albumId);
+		}
 	}	
-	return findSongs($search);
+	pg_free_result($result);
 
+	$songs;
 }
+
 ?>
