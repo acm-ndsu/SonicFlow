0b5e78dd5bd64397780f72721b90c3f82b1d5cab
zeande <zechariah.andersen@gmail.com>
2013-02-06 15:11:19 -0600

    Added functions to interact with the database.

--- assets/includes/sonicflow.php
+++ assets/includes/sonicflow.php
@@ -12,4 +12,9 @@ require_once('assets/includes/self_interaction.php');
 require_once('assets/includes/grooveshark.php');
 require_once('assets/includes/gracenote.php');
 
+$dbconn = pg_connect(getConnectionString());
+pg_prepare($dbconn,'itemCheck','SELECT id FROM $1 WHERE id = $2');
+pg_prepare($dbconn,'addSong',  'INSERT INTO songs   VALUES ($1,$2,$3)');
+pg_prepare($dbconn,'addArtist','INSERT INTO artists VALUES ($1,$2)');
+pg_prepare($dbconn,'addAlbum', 'INSERT INTO albums  VALUES ($1,$2,$3,$4));
 ?>
