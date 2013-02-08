96b08a8a6aab4908ca97648f9c00017e1afd3b46
zeande <zechariah.andersen@gmail.com>
2013-02-08 14:27:14 -0600

    Added a daemon to play music from the queue on the server.

---
+++ daemon.php
@@ -0,0 +1,22 @@
+<?php
+
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
+
+	include("GrooveShark_PHP/grooveshark.class.php");
+	include("assets/includes/sonicflow.php");
+
+	$gs = new GrooveShark();
+	while (true) {
+		$next   = getNext();
+		if ($next == '') {
+			sleep(2);
+		}
+		$id     = $next[0];
+		$songId = $next[1];
+		$data = $gs->getSongById($songId);
+		passthru('wget ' . $data['url'] . ' -O - | mplayer -cache 8192 -');
+		removeSongFromQueue($id);
+	}
+
+?>
