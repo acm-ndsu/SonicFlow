d5c1d7e734c8cdadd9626ae0d24551b80e2a7dc8
zeande <zechariah.andersen@gmail.com>
2013-02-09 19:10:46 -0600

    Added some style to the site. Still needs a lot of work, though.

---
+++ assets/functions.js
@@ -0,0 +1,12 @@
+
+function showQueue() {
+	document.getElementById('page').src = "queue.html";	
+}
+
+function showNowPlaying() {
+	document.getElementById('page').src = "playing.html";
+}
+
+function showSearch() {
+	document.getElementById('page').src = "search.html";
+}
