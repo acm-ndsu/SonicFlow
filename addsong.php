81a81b40af7f7baacab3f77bbdfe3318d1228cf9
zeande <zechariah.andersen@gmail.com>
2013-02-10 12:01:16 -0600

    Removed functionality from addsong.php that showed the queue.

--- addsong.php
+++ addsong.php
@@ -3,33 +3,9 @@
 <?php 
 	if(isset($_POST['id'])) {
 		addSongToQueue($_POST['id']);
-		unset($_POST['id']);
 		echo "Song added!";
 	}
-	$currentQueue = getQueue(); 
-	if (empty($currentQueue)) {
-		echo "<div class=\"emptyQueue\">\n";
-		echo "\tThe queue is currently empty.\n";
-		echo "</div>\n";
-	}
 ?>
-		<div class="queue">
-			<ul class="songs">
-<?php
-			foreach ($currentQueue as $s) {
-?>
-			<li>
-				<img class="albumArt" src="<?php echo $s->arturl; ?>" alt="Song Album" />
-				<div class="songTitle"><?php echo $s->title; ?></div>
-				<div class="songArtist"><?php echo $s->artist ?></div>
-				<div class="songAlbum"><?php echo $s->album; ?></div>
-			</li>
-<?php
-			}
-?>
-	
-			</ul>
-		</div>
 		<script type="text/javascript">
 			function redirect() {
 				window.location = "queue.html";
