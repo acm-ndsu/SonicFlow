481f4084b6ebaa0ecb0b1688464833f7c1f0001a
zeande <zechariah.andersen@gmail.com>
2013-02-10 15:01:25 -0600

    Worked on automatically updating of now playing and queue. Todo: change it so that the content in the page is changed rather than refreshing the page.

--- update.php
+++ update.php
@@ -1,13 +1,29 @@
 <?php require_once("config.php") ?>
-<?php require_once("assets/includes/self_interaction.php");
+<?php require_once("assets/includes/sonicflow.php"); ?>
 <?php
 
 	/*
 	 * This will check whether there is a new song yet.
 	 * Returns true if there is a new song; otherwise, false.
 	 */
-
 	$id = $_POST['id'];
-	$song = getNext();
-	echo ($id != $song->id);
+	$from = $_POST['from'];
+	if ($id == "none") {
+		echo 0;
+	}
+	if ($from == "playing") {
+		$song = getNext();
+		if ($id != $song->id) {
+			echo 1;
+//			$content = "\t\t\t\t\t<li class=\"songTitle\">$song->title</li>\n"
+//				. "\t\t\t\t\t<li id=\"songId\" style=\"display:none\">$song->id</li>\n"
+//				. "\t\t\t\t\t<li class=\"songArtist\">$song->artist</li>\n"
+//				. "\t\t\t\t\t<li class=\"songAlbum\">$song->album</li>";
+//			echo $content;
+		} else {
+			echo 0;
+		}
+	} else {
+		// Logic for handling queue	
+	}
 ?>
