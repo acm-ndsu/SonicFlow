140b3035d1c4a2985a1a75ff8f4f80b8c9b6d2fe
SonicFlow <zechariah.andersen@gmail.com>
2013-02-12 08:27:56 -0600

    Added functionality to hide header/footer.

--- assets/functions.js
+++ assets/functions.js
@@ -15,3 +15,24 @@ function submit(id) {
 	document.getElementById(id).submit();
 }
 
+function maximize() {
+	decreaseSize($("#header").height(),$("#footer").height());
+	window.scrollTo(0,120-$("#header").height());
+	$("body").css("overflow","hidden");
+}
+
+function decreaseSize(head,foot) {
+	if (head > 0) {
+		$("#header").height(head-2);
+		$(".content").css("padding-top",Math.min($("#currentSong").css("padding-top") + 1,40));
+		$("#topNav").css("z-index",-1);
+	}
+	if (foot > 10) {
+		$("#footer").height(foot-1);
+		$("#gsImg").height($("#gsImg").height()-1);
+		
+	}
+	if (head > 0 || foot > 10) {
+		setTimeout(maximize,1);
+	}
+}
