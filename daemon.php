20dff0e556b809522a0a1d055aa3187d4ec60722
SonicFlow <zechariah.andersen@gmail.com>
2013-02-11 19:47:23 -0600

    Updated daemon. It is now more robust and doesn't need to be ran within a bash while loop.

--- daemon.php
+++ daemon.php
@@ -10,20 +10,26 @@
 	while (true) {
 		try {
 			$next   = getNext();
-			if ($next == '') {
+			if ($next[0] == '') {
 				sleep(2);
 			} else {
 				$id = $next[0];
 				$song = $next[1];
 				$songId = $song->id;
 				$data = $gs->getSongById($songId);
-				passthru('wget ' . $data['url'] . ' -O - | mplayer -cache 8192 -af volnorm=2:1.0 -');
-				removeSongFromQueue($id);
+				if ($data != null)  {
+					passthru('wget ' . $data['url'] . ' -O - | mplayer -cache 8192 -');
+					removeSongFromQueue($id);
+				} else {
+					echo "\n\nFailed to retrieve URL for $song->title by $song->artist!\n\n";
+					sleep(2);
+				}
 		
 			}
 		} catch (Exception $e) { 
-			echo "\nError happened!\n";
-			echo "\n$e\n";
+			echo "\n\nException happened! with $song->title by $song->artist\n\n";
+			$gs = new GrooveShark();
+			sleep(2);
 		}
 	}
 
