a9435b21857c640763e99c34f549dcba98ebd509
SonicFlow <zechariah.andersen@gmail.com>
2013-02-11 22:18:37 -0600

    Added some interface changes.

--- results.php
+++ results.php
@@ -42,23 +42,25 @@
 			<p><span id="resultCount"><?php echo $numResults; ?></span>
 			results in <?php echo $providerName; ?> for
 			"<span id="searchPhrase"><?php echo $search; ?></span>":</p>
-			<ul class="songs">
+			<div class="results">
+				<ul class="songs">
 <?php
 			foreach ($searchResults as $s) {
 ?>
-				<li onclick="submit(<?php echo $s->id; ?>);">
-					<img class="albumArt" src="<?php echo getArtLoc($s->albumId); ?>" alt="Song Album" />
-					<div class="songTitle"><?php echo $s->title; ?></div>
-					<div class="songArtist"><?php echo $s->artist; ?></div>
-					<div class="songAlbum"><?php echo $s->album; ?></div>
-					<form id="<?php echo $s->id;?>" action="addsong.php" method="post">
-						<input type="hidden" name="id" value="<?php echo $s->id; ?>" />
-					</form>
-				</li>
+					<li onclick="submit(<?php echo $s->id; ?>);">
+						<img class="albumArt" src="<?php echo getArtLoc($s->albumId); ?>" alt="Song Album" />
+						<div class="songTitle"><?php echo $s->title; ?></div>
+						<div class="songArtist"><?php echo $s->artist; ?></div>
+						<div class="songAlbum"><?php echo $s->album; ?></div>
+						<form id="<?php echo $s->id;?>" action="addsong.php" method="post">
+							<input type="hidden" name="id" value="<?php echo $s->id; ?>" />
+						</form>
+					</li>
 <?php
 			}
 ?>
-			</ul>
+				</ul>
+			</div>
 <?php
 	}
 	if ($provider == 'sonicflow') {
