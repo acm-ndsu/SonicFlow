b59d8ca7be83667893f2395ac638628b67c69c61
zeande <zechariah.andersen@gmail.com>
2013-03-06 08:54:19 -0600

    Added API change.

--- api.php
+++ api.php
@@ -1,24 +1,25 @@
 <?php
 	$action = $_POST["action"];
+	$channel = "Headphone";
 	switch ($action) {
 	case "vup":
-		system("amixer -q set Master 2- unmute");
-		echo "Volume -2";
-		break;
-	case "vdown":
-		system("amixer -q set Master 2+ unmute");
+		system("amixer -q set $channel 2+ unmute");
 		echo "Volume +2";
 		break;
+	case "vdown":
+		system("amixer -q set $channel 2- unmute");
+		echo "Volume -2";
+		break;
 	case "toggle":
-		system("amixer -q set Master toggle");
+		system("amixer -q set $channel toggle");
 		echo "Mute option toggled";
 		break;
 	case "mute":
-		system("amixer -q set Master mute");
+		system("amixer -q set $channel mute");
 		echo "Volume muted";
 		break;
 	case "umute":
-		system("amixer -q set Master unmute");
+		system("amixer -q set $channel unmute");
 		echo "Volume unmuted";
 		break;
 	}
