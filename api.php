56e917908e24556de5c851b9e1dcfccc88044f58
Cesar Ramirez <ramirez@cesarandres.com>
2013-04-11 14:14:58 -0500

    Changed the amixer command to acomodate channels

--- api.php
+++ api.php
@@ -1,25 +1,26 @@
 <?php
 	$action = $_POST["action"];
-	switch ($action) {
-	case "vup":
-		system("amixer -q set Master 2- unmute");
-		echo "Volume -2";
-		break;
-	case "vdown":
-		system("amixer -q set Master 2+ unmute");
-		echo "Volume +2";
-		break;
-	case "toggle":
-		system("amixer -q set Master toggle");
-		echo "Mute option toggled";
-		break;
-	case "mute":
-		system("amixer -q set Master mute");
-		echo "Volume muted";
-		break;
-	case "umute":
-		system("amixer -q set Master unmute");
-		echo "Volume unmuted";
-		break;
-	}
+        $channel = "Headphone";
+        switch ($action) {
+        case "vup":
+                system("amixer -q set $channel 2+ unmute");
+                echo "Volume +2";
+                break;
+        case "vdown":
+                system("amixer -q set $channel 2- unmute");
+                echo "Volume -2";
+                break;
+        case "toggle":
+                system("amixer -q set $channel toggle");
+                echo "Mute option toggled";
+                break;
+        case "mute":
+                system("amixer -q set $channel mute");
+                echo "Volume muted";
+                break;
+        case "umute":
+                system("amixer -q set $channel unmute");
+                echo "Volume unmuted";
+                break;
+        }
 ?> 
