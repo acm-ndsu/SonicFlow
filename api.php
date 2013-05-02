2493e63091dcb2501ef708ecb0ab02f73689cc95
Cesar Ramirez <ramirez@cesarandres.com>
2013-05-02 13:26:22 +0000

    api.php now outputs JSON, not tested yet

--- api.php
+++ api.php
@@ -1,26 +1,55 @@
 <?php
+	require_once('assets/includes/sonicflow.php'); ?>
+
 	$action = $_POST["action"];
+	$search = $_POST["search"];
 	$channel = "Headphone";
+	
+	$result = "{\"action\":$action,\"result\":{";
+
 	switch ($action) {
 	case "vup":
 		system("amixer -q set $channel 2+ unmute");
-		echo "Volume +2";
+		$result = $result."\"change\":\"+2\"";
 		break;
 	case "vdown":
 		system("amixer -q set $channel 2- unmute");
-		echo "Volume -2";
+		$result = $result."\"change\":\"-2\"";
 		break;
 	case "toggle":
 		system("amixer -q set $channel toggle");
-		echo "Mute option toggled";
+		$result = $result."\"change\":\"toggle\"";
 		break;
 	case "mute":
 		system("amixer -q set $channel mute");
-		echo "Volume muted";
+		$result = $result."\"change\":\"mute\"";
 		break;
 	case "umute":
 		system("amixer -q set $channel unmute");
-		echo "Volume unmuted";
+		$result = $result."\"change\":\"unmute\"";
+		break;
+	case "search":
+		$searchResults = getSonicFlowResults($search);
+		if (count($searchResults) == 0) {
+			$provider = "grooveshark";
+			$searchResults = getGroovesharkResults($search);
+		}
+		$numResults = count($searchResults);
+
+		$result = $result . "\"size\":".$numResults.",\"provider\":".$providerName."\",results\":{";
+
+		if (is_null($searchResults)) {
+			break;
+		} 
+
+		foeach ($searchResults as $s) {
+			$result = $result . "\"id:\"".$s->id.",\"title:\"".$s->title.",\"artist\"".$s->artist.",\"album\"".$s->album;
+		}
+		$result = $result . "}"
 		break;
 	}
+
+	$result = $result."}}";
+
+	echo $result;
 ?> 
