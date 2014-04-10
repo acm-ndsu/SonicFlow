5fd6b1115b64e675e92059bf6a4f33239066e8ed
TF Nelson <tails.fanatic@gmail.com>
2014-04-09 20:04:25 -0500

    Added pygrooveshark interface

---
+++ assets/includes/pygs/play.py
@@ -0,0 +1,12 @@
+import grooveshark
+import grooveshark.classes.song
+import sys
+import subprocess
+
+client = grooveshark.Client()
+client.init()
+
+resp = {'Name': sys.argv[0], 'SongID': sys.argv[1]}
+s = grooveshark.classes.song.Song.fromResponse(resp, client.connection)
+
+subprocess.call(['mplayer', song.stream.url])
