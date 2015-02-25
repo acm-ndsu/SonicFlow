5fd88e86214e6bfd435bc71c25fc02e9239e5853
Rebecca Nelson <tails.fanatic@gmail.com>
2015-02-25 13:56:37 -0600

    fixed cookie having incorrect timestamp

--- addsong.php
+++ addsong.php
@@ -1,22 +1,27 @@
+<?php
+	if(!isset($_COOKIE['setmark'])) {
+		$nametag = uniqid();
+		setcookie('setmark', $nametag, time()+86400000, '/');
+		print_r($_COOKIE);
+	} else {
+		$nametag = $_COOKIE['setmark'];
+	}
+
+?>
 <?php include('header.html'); ?>
 <?php require_once('assets/includes/sonicflow.php'); ?>
 <?php 
 	
-	if(!isset($_COOKIE['setmark'])) {
-		$nametag = uniqid();
-		setcookie('setmark', $nametag, 86400000);
-	} else {
-		$nametag = $_COOKIE['setmark'];
-	}
 	if(isset($_POST['id'])) {
 		$id = $_POST['id'];
 		$added = addSongToQueue($id);
-		$logfile = fopen('/var/www/SonicFlow/ip.log', 'a');
-		fwrite($logfile, date(DATE_ISO8601) . ' - ' . $id . ' added from ' . $_SERVER['REMOTE_ADDR'] . " {".$nametag."}\n");
-		fclose($logfile);
 		unset($_POST['id']);
 		if ($added == R_SUCCESS) {
 			echo "Song added!";
+			$logfile = fopen('/var/www/SonicFlow/ip.log', 'a');
+			fwrite($logfile, date(DATE_ISO8601) . ' - ' . $id . ' added from ' . $nametag . ' (' . $_SERVER['REMOTE_ADDR'] . ')'."\n");
+			fclose($logfile);
+
 		} else if ($added == R_SONG_REQUEST_TOO_SOON) {
 			$timeSince = time() - getSongRequestTime($id);
 			$t = ceil((SONG_REQUEST_LIMIT - $timeSince) / 60);
