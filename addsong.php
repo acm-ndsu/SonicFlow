538f867adebd907ce28fdfbde918f3a6f3f76f1b
Rebecca Nelson <tails.fanatic@gmail.com>
2015-03-04 13:55:14 -0600

    Straight-up blocking songs

--- addsong.php
+++ addsong.php
@@ -1,33 +1,28 @@
 <?php
-	if(!isset($_COOKIE['setmark'])) {
-		$nametag = uniqid();
-		setcookie('setmark', $nametag, time()+86400000, '/');
-		print_r($_COOKIE);
-	} else {
-		$nametag = $_COOKIE['setmark'];
-	}
 
+$BANNED_SONGS = array(41961821, 41884141);
+include('tags.php');
 ?>
 <?php include('header.html'); ?>
 <?php require_once('assets/includes/sonicflow.php'); ?>
 <?php 
 	
 	if(isset($_POST['id'])) {
-		$id = $_POST['id'];
-		$added = addSongToQueue($id);
-		unset($_POST['id']);
-		if ($added == R_SUCCESS) {
-			echo "Song added!";
-			$logfile = fopen('/var/www/SonicFlow/ip.log', 'a');
-			fwrite($logfile, date(DATE_ISO8601) . ' - ' . $id . ' added from ' . $nametag . ' (' . $_SERVER['REMOTE_ADDR'] . ')'."\n");
-			fclose($logfile);
-
-		} else if ($added == R_SONG_REQUEST_TOO_SOON) {
-			$timeSince = time() - getSongRequestTime($id);
-			$t = ceil((SONG_REQUEST_LIMIT - $timeSince) / 60);
-			$s = ($t != 1) ? 's' : '';
-			echo "Song requested too soon. It can be requested " .
-					"again in $t minute$s";
+		if (in_array($_POST['id'], $BANNED_SONGS)) {
+			echo 'This song has been banned. Please stop.';
+		} else {
+			$id = $_POST['id'];
+			$added = addSongToQueue($id);
+			unset($_POST['id']);
+			if ($added == R_SUCCESS) {
+				echo "Song added!";
+			} else if ($added == R_SONG_REQUEST_TOO_SOON) {
+				$timeSince = time() - getSongRequestTime($id);
+				$t = ceil((SONG_REQUEST_LIMIT - $timeSince) / 60);
+				$s = ($t != 1) ? 's' : '';
+				echo "Song requested too soon. It can be requested " .
+						"again in $t minute$s";
+			}
 		}
 	}
 ?>
--- scripts/reset_queue.php
+++ scripts/reset_queue.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php require('assets/includes/sonicflow.php');
-
-$q = getQueue();
-while (!empty($q)) {
+do {
+	$q = getQueue();
+	print_r($q);
 	removeSongAtPosition(0, false);
-}
+} while (!empty($q));
 
 ?> 
